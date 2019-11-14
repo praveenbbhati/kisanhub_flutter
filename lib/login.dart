ce8e75bf281b0a3826041c291baa1167dfb07af3
Praveen <praveenbhati1992@gmail.com>
2019-11-14 23:35:06 +0530

    login validation done

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'utils/Validator.dart';
+import 'models/LoginRequest.dart';
 
 class LoginPage extends StatefulWidget{
  
@@ -11,74 +13,117 @@ class LoginPage extends StatefulWidget{
 
 class _LoginPageState extends State<LoginPage>{
 
+  GlobalKey<FormState> _key = GlobalKey();
+  bool _validate = false;
+  LoginRequest _loginRequest = LoginRequest();
+
+
+  Widget getFormUI(){
+
+    final emailField = TextFormField(
+      obscureText: false,
+      style: TextStyle(fontSize: 20.0),
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Email",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))
+      ),
+      validator: Validator().validateEmail,
+      onSaved: (String value){
+        _loginRequest.email = value;
+      },
+    );
+
+    final passwordField = TextFormField(
+      obscureText: true,
+      style: TextStyle(fontSize: 20.0),
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Password",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))
+      ),
+      validator: Validator().validatePassword,
+      onSaved: (String value){
+        _loginRequest.password = value;
+      },
+    );
+
+    final loginButon = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Color(0xffC6D32D),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: _sendToServer,
+        child: Text("Login",
+            textAlign: TextAlign.center,
+            style:TextStyle(fontSize: 20.0,color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+
+    return  Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        SizedBox(
+          height: 155.0,
+          child: Image.asset(
+            "assets/kisanhub_logo.png",
+          ),
+        ),
+        SizedBox(height: 45.0),
+        emailField,
+        SizedBox(height: 25.0),
+        passwordField,
+        SizedBox(
+          height: 35.0,
+        ),
+        loginButon,
+        SizedBox(
+          height: 15.0,
+        ),
+      ],
+    );
+  }
+
+  _sendToServer() {
+    if (_key.currentState.validate()) {
+      // No any error in validation
+      _key.currentState.save();
+      print("Email ${_loginRequest.email}");
+      print("Password ${_loginRequest.password}");
+    } else {
+      // validation error
+      setState(() {
+        _validate = true;
+      });
+    }
+  }
+ 
+
+
   @override
   Widget build(BuildContext context){
 
-    final emailField = TextField(
-          obscureText: false,
-          style: TextStyle(fontSize: 20.0),
-          keyboardType: TextInputType.emailAddress,
-          decoration: InputDecoration(
-              contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-              hintText: "Email",
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-        );
-        final passwordField = TextField(
-          obscureText: true,
-          style: TextStyle(fontSize: 20.0),
-          decoration: InputDecoration(
-              contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-              hintText: "Password",
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-        );
-        final loginButon = Material(
-          elevation: 5.0,
-          borderRadius: BorderRadius.circular(30.0),
-          color: Color(0xffC6D32D),
-          child: MaterialButton(
-            minWidth: MediaQuery.of(context).size.width,
-            padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-            onPressed: () {},
-            child: Text("Login",
-                textAlign: TextAlign.center,
-                style:TextStyle(fontSize: 20.0,color: Colors.white, fontWeight: FontWeight.bold)),
+    return Scaffold(
+      body: Center(
+        child: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(36.0),
+            child: new Form(
+              key: _key,
+              autovalidate: _validate,
+              child: getFormUI(),
+            ) 
           ),
-        );
-
-        return Scaffold(
-          body: Center(
-            child: Container(
-              color: Colors.white,
-              child: Padding(
-                padding: const EdgeInsets.all(36.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    SizedBox(
-                      height: 155.0,
-                      child: Image.asset(
-                        "assets/kisanhub_logo.png",
-                      ),
-                    ),
-                    SizedBox(height: 45.0),
-                    emailField,
-                    SizedBox(height: 25.0),
-                    passwordField,
-                    SizedBox(
-                      height: 35.0,
-                    ),
-                    loginButon,
-                    SizedBox(
-                      height: 15.0,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        );   
+        ),
+      ),
+    );   
   }
+
 }
---
+++ lib/utils/Validator.dart
@@ -0,0 +1,28 @@
+class Validator{
+  static Validator _validator;
+
+  factory Validator() => _validator ??= new Validator._();
+
+  Validator._();
+
+  String validatePassword(String value) {
+    if (value.isEmpty) {
+      return "Password is Required";
+    }
+    return null;
+  }
+
+  String validateEmail(String value) {
+    String pattern =
+        r'^(([^&lt;&gt;()[\]\\.,;:\s@\"]+(\.[^&lt;&gt;()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regExp = new RegExp(pattern);
+    if (value.isEmpty) {
+      return "Email is Required";
+    } else if (!regExp.hasMatch(value)) {
+      return "Invalid Email";
+    } else {
+      return null;
+    }
+  }
+
+}
